6fd45ff7c7de52f8f3573234b996ec3840164d26
Gabrielhaj <g171859@dac.unicamp.br>
2023-08-31 21:57:15 -0300

    FirstCommit

---
+++ Core/Inc/main.h
@@ -0,0 +1,101 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define B1_EXTI_IRQn EXTI15_10_IRQn
+#define LeftMotorPWM_Pin GPIO_PIN_0
+#define LeftMotorPWM_GPIO_Port GPIOC
+#define RightMotorPWM_Pin GPIO_PIN_1
+#define RightMotorPWM_GPIO_Port GPIOC
+#define LPUART1_TX_Pin GPIO_PIN_2
+#define LPUART1_TX_GPIO_Port GPIOA
+#define LPUART1_RX_Pin GPIO_PIN_3
+#define LPUART1_RX_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define LeftMotorIn1_Pin GPIO_PIN_12
+#define LeftMotorIn1_GPIO_Port GPIOB
+#define LeftMotorIn1_EXTI_IRQn EXTI15_10_IRQn
+#define RightMotorIn4_Pin GPIO_PIN_10
+#define RightMotorIn4_GPIO_Port GPIOA
+#define RightMotorIn4_EXTI_IRQn EXTI15_10_IRQn
+#define T_SWDIO_Pin GPIO_PIN_13
+#define T_SWDIO_GPIO_Port GPIOA
+#define T_SWCLK_Pin GPIO_PIN_14
+#define T_SWCLK_GPIO_Port GPIOA
+#define T_SWO_Pin GPIO_PIN_3
+#define T_SWO_GPIO_Port GPIOB
+#define LeftWheelEnc_Pin GPIO_PIN_4
+#define LeftWheelEnc_GPIO_Port GPIOB
+#define RightWheelEnc_Pin GPIO_PIN_5
+#define RightWheelEnc_GPIO_Port GPIOB
+#define RightMotorIn3_Pin GPIO_PIN_7
+#define RightMotorIn3_GPIO_Port GPIOB
+#define LeftMotorIn2_Pin GPIO_PIN_9
+#define LeftMotorIn2_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
